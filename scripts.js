ec08d1cb48133a864dc72ce68ddc2e26a0565201
Michał Matysiak <michalmatysiak02@gmail.com>
2018-03-11 13:28:42 +0100

    Quote prompt

--- scripts.js
+++ scripts.js
@@ -5,8 +5,8 @@
   
   
 
-var a = prompt('Please enter variable a :'),
-    h = prompt('Please specify variable h:'),
-    triangleArea = a*h/2;
-alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
+// var a = prompt('Please enter variable a :'),
+//     h = prompt('Please specify variable h:'),
+//     triangleArea = a*h/2;
+// alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+// console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
