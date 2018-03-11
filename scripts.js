08326f5d9e5b224c3f97125b346570cf8950562b
Michał Matysiak <michalmatysiak02@gmail.com>
2018-03-11 13:20:29 +0100

    Add index.html and scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+ var a = 3,
+  	 h = 6, 
+     triangleArea = a*h/2 
+  console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+  
+  
+
+var a = prompt('Please enter variable a :'),
+    h = prompt('Please specify variable h:'),
+    triangleArea = a*h/2;
+alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea); 
